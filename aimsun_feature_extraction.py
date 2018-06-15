0e761fc6878e988f7eff5e038d0358f873c09623
Serena Hughes <sjhughes@berkeley.edu>
2018-06-15 11:45:32 -0700

    heading added, sections are dictionaries, units fixed

--- aimsun_feature_extraction.py
+++ aimsun_feature_extraction.py
@@ -10,11 +10,15 @@ import sys
 import csv
 import os
 import json
+import math
 
 WINDOWS_ENCODING = '\\'
 UNIX_ENCODING = '/'
 
 SYSTEM_TYPE = 'windows'
+
+MPH_CONSTANT = 0.62137119 #multiply km/hr to convert to mph
+
 
 def buildGeolocation(translator, coordinate_pair):
     """
@@ -23,6 +27,15 @@ def buildGeolocation(translator, coordinate_pair):
     """
     coordinate = translator.toDegrees(coordinate_pair)
     return {'lat': coordinate.x, 'lon': coordinate.y}
+
+def getHeading(origin, destination):
+    """
+    Computes the heading of a section in degrees relative to the x axis
+    given the geolocation endpoints of the section as dictionaries of lat and lon.
+    """
+    dy = destination['lon'] - origin['lon']
+    dx = destination['lat'] - origin['lat']
+    return math.atan2(dy, dx) * 180 / math.pi
 
 def buildJunctions(model):
     """
@@ -33,6 +46,7 @@ def buildJunctions(model):
         """
         Builds a dictionary containing information about a turn object.
         Inherits model from parent function buildJunctions.
+        Units for turn speed is mph.
         """
         turn_map = {}
         turn_map['turnID'] = turn_object.getId()
@@ -49,8 +63,7 @@ def buildJunctions(model):
         num_destination_lanes = len(destination_object.getLanes())
         turn_map['toLaneRange'] = num_destination_lanes - turn_object.getDestinationToLane(), num_destination_lanes - turn_object.getDestinationFromLane()
 
-        TURN_SPEED_CONSTANT = 0.621371
-        turn_map['speed'] = turn_object.getSpeed() * TURN_SPEED_CONSTANT
+        turn_map['speed'] = turn_object.getSpeed() * MPH_CONSTANT
         turn_map['angle'] = turn_object.calcAngleBridge()
         turn_map['type'] = turn_object.getDescription()
 
@@ -85,6 +98,7 @@ def buildSections(model):
     def buildLane(lane_index):
         """
         Returns a dictionary mapping keys to the values in a lane object.
+        Units for length and width are feet and speed is mph.
         """
         LANE_CONST = 3.28084
         lane = {}
@@ -97,13 +111,14 @@ def buildSections(model):
 
     print 'Building section map'
     numSections = 0
-    sections = []
+    sections = {}
 
     for types in model.getCatalog().getUsedSubTypesFromType(model.getType("GKSection")):
         numSections += len(types)
         for section_object in types.itervalues():
             section = {}
-            section['sectionID'] = section_object.getId()
+            sectionID = section_object.getId()
+            section['sectionID'] = sectionID
             section['name'] = section_object.getName()
             section['externalID'] = section_object.getExternalId()
             section['speed'] = section_object.getSpeed()
@@ -117,7 +132,15 @@ def buildSections(model):
             translator = GKCoordinateTranslator(model)
             section['shape'] = [buildGeolocation(translator, point) for point in shape]
 
-            sections.append(section)
+            previous = section['shape'][0]
+            previous['heading'] = None
+
+            for i in range (1, len(shape)):
+                curr = section['shape'][i]
+                curr['heading'] = getHeading(previous, curr)
+                previous = curr
+
+            sections[sectionID] = section
     print 'Finished building section map'
     return {'numSections': numSections, 'sections': sections}
 
@@ -127,8 +150,8 @@ def buildJSON(model, path):
     junction_map = buildJunctions(model)
     section_map = buildSections(model)
 
-    junction_json = json.dumps(junction_map, indent=2, sort_keys=False)
-    section_json = json.dumps(section_map, indent=2, sort_keys=False)
+    junction_json = json.dumps(junction_map, indent=2)
+    section_json = json.dumps(section_map, indent=2)
 
     junction_path = path + separator() + 'junction.json'
     print 'Writing', junction_path
@@ -148,6 +171,6 @@ def separator():
 gui=GKGUISystem.getGUISystem().getActiveGui()
 model = gui.getActiveModel()
 
-path='L:/arterial_detector_analysis_files'
+path='C:\Users\Serena\connected_corridors\extracted_aimsun_data'
 
 buildJSON(model, path)
