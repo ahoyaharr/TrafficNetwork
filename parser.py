23b80a8de3eb566aaf985d7a66f2c415516f164b
ahoyaharr <alexander.harrison@berkeley.edu>
2018-06-18 14:15:19 -0700

    formatting

--- parser.py
+++ parser.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
+
 def separator():
     UNIX_ENCODING = '/'
     return UNIX_ENCODING
+
 
 def get_files(path='data', absolute=False):
     # Returns a list of paths to uncorrected files in a directory.
@@ -16,13 +18,14 @@ def get_files(path='data', absolute=False):
     files = [file for file in os.listdir(get_script_path(path)) if '.json' in file]
     return [get_script_path(path) + separator() + file for file in files] if absolute else files
 
+
 def read_file(s):
     file = open(os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + 'data' + separator() + s)
     return file.read()
 
 
 def get_JSON_strings():
-	""" 
-	Maps the file name without the extension to the associated JSON string.
-	"""
-	return {file.rsplit(".", 1)[0]: read_file(file) for file in get_files()} 
+    """
+    Maps the file name without the extension to the associated JSON string.
+    """
+    return {file.rsplit(".", 1)[0]: read_file(file) for file in get_files()}
