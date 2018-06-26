9caa84136cb4f0867f6776b609a4bfb464687326
ahoyaharr <alexander.harrison@berkeley.edu>
2018-06-26 13:25:23 -0700

    generic readfile for parsing probe data

--- util/parser.py
+++ util/parser.py
@@ -7,25 +7,24 @@ def separator():
     return UNIX_ENCODING
 
 
-def get_files(path='data', absolute=False):
+def get_script_path(p):
+    return os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + p
+
+def get_JSON_files(path='data', absolute=False):
     # Returns a list of paths to uncorrected files in a directory.
     # path: the path to the directory
     # absolute: whether or not the filePath should be relative, i.e. ~/myFile.file vs. ~/.../myFile.file
     # system_type: 'windows' or 'unix'
-    def get_script_path(p):
-        return os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + p
-
     files = [file for file in os.listdir(get_script_path(path)) if '.json' in file]
     return [get_script_path(path) + separator() + file for file in files] if absolute else files
 
-
-def read_file(s):
-    file = open(os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + 'data' + separator() + s)
+def read_file(s, dir='data'):
+    file = open(os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + dir + separator() + s)
     return file.read()
-
 
 def get_JSON_strings():
     """
     Maps the file name without the extension to the associated JSON string.
     """
-    return {file.rsplit(".", 1)[0]: read_file(file) for file in get_files()}
+    return {file.rsplit(".", 1)[0]: read_file(file) for file in get_JSON_files()}
+
