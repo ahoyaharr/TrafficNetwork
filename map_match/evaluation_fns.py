42d34ea9c7e05ba23ca6165f75089d6bb9b9694b
ahoyaharr <alexander.harrison@berkeley.edu>
2018-08-07 14:25:14 -0700

    implement viterbi optimization, does not give correct result currently

--- map_match/evaluation_fns.py
+++ map_match/evaluation_fns.py
@@ -1,16 +1,83 @@
 import copy
 import math
+import util.utils as utils
+from collections import namedtuple
+from heapq import heapify, heappop as pop, heappush as push
+from functools import reduce
+
 
 def viterbi_optimized(network, scores):
     """
     An optimized version of Viterbi. Instead of evaluating the K*O matrix column by column, the single most likely
     path is evaluated. In the worst case, the full matrix is evaluated which yields the same runtime as the
-    basic Viterbu algorithm, O(K^2 * O). In the best case, only a single path is evaluated, which has a time complexity
+    basic Viterbi algorithm, O(K^2 * O). In the best case, only a single path is evaluated, which has a time complexity
     of Ω(K*O).
     :param network:
     :param scores:
     :return:
     """
+    def normalize_subpath_scores(subpaths):
+        """
+        normalizes the scores of all paths in a subpath
+        :param subpaths:
+        :return:
+        """
+        sum_of_scores = reduce(lambda total, t: total + t.score, subpaths, 0)
+        return [subpath_factory(-(path.score / sum_of_scores), path.length, path.path) for path in subpaths]
+
+    def update(active_subpath):
+        """
+        Given a single active path, find the most probable next candidate and add it to the path.
+        :param active_subpath: a named tuple with fields score, length, path
+        :return: an active subpath with the most probable candidate appended to it
+        """
+
+        def update_pr(candidate):
+            """
+            Finds the probability of an active path if the candidate is added to it.
+            Score = Probability of active path * Probability of candidate / Distance(old path -> candidate)
+            :param candidate: tuple of (id of candidate vertex, score)
+            :return: score
+            """
+            candidate_probability = candidate[1]
+            distance = 1 + network.shortest_distance_between_vertices(active_subpath.path[-1], candidate[0])
+            #print('[candidate: {0}], [active_subpath: {1}], [distance: {2}]'.format(candidate, active_subpath, distance))
+            return active_subpath.score * candidate_probability / distance ** 1.5
+
+        def update_path(candidate):
+            """
+            Given an active path, finds the most probable path with the candidate added to it.
+            :param candidate: tuple of (id of candidate vertex, score)
+            :return: A list of vertex IDs
+            """
+            return active_subpath.path[:-1] + network.find_vertex_path(active_subpath.path[-1], candidate[0], False)[0]
+
+        active_candidates = list(scores[active_subpath.length].items())
+        best_candidate = min(active_candidates, key=update_pr)
+        # print('best candidate for {0} is {1} with score {2}'.format(active_subpath, best_candidate, update_path(best_candidate)))
+        return subpath_factory(update_pr(best_candidate), active_subpath.length + 1, update_path(best_candidate))
+
+    subpath_factory = namedtuple('Subpath', ['score', 'length', 'path'])
+
+    """ At the first observation, the possible paths are the candidates, and their emission probabilities. 
+    Score is multiplied by -1 because heapq is a min heap. """
+    active_subpaths = [subpath_factory(-score, 1, [candidate]) for candidate, score in scores[0].items()]
+    heapify(active_subpaths)
+
+    """ Add the most probable candidate to the most probable path, until a complete path is found. """
+    best_subpath = pop(active_subpaths)
+    while best_subpath.length < len(scores):
+        print('active_subpaths: {0}, best_subpath: {1}, {2}'.format(len(active_subpaths)+1, best_subpath.score, best_subpath.length))
+        best_subpath = update(best_subpath)
+        if best_subpath.score == 0:
+            print('warning: best_subpath_score is {0}'.format(best_subpath.score))
+        push(active_subpaths, best_subpath)
+        active_subpaths = normalize_subpath_scores(active_subpaths)
+        best_subpath = pop(active_subpaths)
+
+    """ Return the most probable complete path. """
+    return best_subpath.path
+
 
 def viterbi(network, scores):
     """
@@ -60,9 +127,10 @@ def viterbi(network, scores):
 
     """ At the first observation, the possible paths are the candidates, and their emission probabilities. """
     possible_paths = [([candidate], score) for candidate, score in scores[0].items()]
-
+    utils.print_progress(len(scores), prefix='searching for most probable route')
     """ For each observation, update the paths and probabilities. """
     for candidate_map in scores[1:]:
+        utils.print_progress(len(scores), prefix='searching for most probable route')
         possible_paths = [find_next_step((candidate, score), possible_paths)
                           for candidate, score in candidate_map.items()]
         sum_of_scores = sum(path[1] for path in possible_paths)
