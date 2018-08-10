7983a9bbcc3fc9ec0a2de3ca04b35a8dad84502a
Serena <sjhughes@berkeley.edu>
2018-08-10 11:44:12 -0700

    Changed the way offsetting heading works. Now, offset the heading uniformly by x degrees, then noise can be added randomly to further offset. Optimized imports.

--- util/artificial_paths.py
+++ util/artificial_paths.py
@@ -1,13 +1,17 @@
 import datetime
-import random
-import graph_tool
-import util.Shapes
-import util.utils
 import math
+from random import randrange
+from random import sample
+
+import graph_tool
+
+from util.Shapes import Point, DataPoint
+from util.utils import offset_point
 
 
 def generate_path(network, section1=None, section2=None, min_path_length=30000, max_path_length=75000,
-                  max_offset_distance=500, max_offset_heading=25, omit_factor=1):
+                  max_offset_distance=500, random_distance=True, offset_heading=10, heading_noise=5,
+                  omit_factor=1):
     """
     Finds a path of DataPoints through the network from section1 to section2, or between two random sections, within
     a provided range for acceptable length. Skews the points randomly under a threshold to add noise to the data.
@@ -17,19 +21,21 @@ def generate_path(network, section1=None, section2=None, min_path_length=30000, 
     :param min_path_length: minimum allowable length of path, in feet.
     :param max_path_length: maximum allowable length of path, in feet.
     :param max_offset_distance: the max number of feet that a point's lon/lat can be offset from the true path.
-    :param max_offset_heading: the max number of degrees that a point's heading can be offset from the true heading.
+    :param random_distance: if True, offset distances will be randomized.
+    :param offset_heading: the number of degrees that a point's heading will be offset from the true heading.
+    :param heading_noise: if
     :param omit_factor: the integer x where you would like to include every xth point. If x is 1, all points included.
     :return: two lists of DataPoints, one with noise and one of the true path.
     """
 
     def random_vertex(exclude=-1):
-        return random.sample(vertices - {exclude}, 1)[0]
+        return sample(vertices - {exclude}, 1)[0]
 
     def length(vp):
         return network.shortest_distance_between_vertices(vp[0], vp[-1]) if vp != [] else 0
 
     def near_junction(v):
-        # Return true if near a junction
+        # Return True if near a junction.
         out_neighbors = [network.junctions[w] for w in [i for i in v.out_neighbors()]]
         in_neighbors = [network.junctions[x] for x in [j for j in v.in_neighbors()]]
         return any(out_neighbors) or any(in_neighbors) or network.junctions[v]
@@ -38,7 +44,8 @@ def generate_path(network, section1=None, section2=None, min_path_length=30000, 
     vertices = {v for v in network.graph.vertices() if cc[v] and not near_junction(v)}
 
     section1 = graph_tool.util.find_vertex(network.graph, network.node_id, section1) if section1 else random_vertex()
-    section2 = graph_tool.util.find_vertex(network.graph, network.node_id, section2) if section2 else random_vertex(section1)
+    section2 = graph_tool.util.find_vertex(network.graph, network.node_id, section2) if section2 else random_vertex(
+        section1)
 
     # Find path between two vertices that satisfies length requirements.
     vertex_path = network.find_vertex_path(section1, section2, True)[0]
@@ -55,8 +62,8 @@ def generate_path(network, section1=None, section2=None, min_path_length=30000, 
     if path_length > max_path_length:
         appx_node_dist = path_length / len(vertex_path)
         # Ceiling divide by the approximate distance between two vertices to determine how many vertices to trim.
-        #diff = -(-(path_length - max_path_length) // appx_node_dist)
-        diff = math.ceil((path_length - max_path_length)/appx_node_dist)
+        # diff = -(-(path_length - max_path_length) // appx_node_dist)
+        diff = math.ceil((path_length - max_path_length) / appx_node_dist)
         vertex_path = vertex_path[:int(len(vertex_path) - diff)]
 
     vertex_path = [v for v in vertex_path if v in vertices]
@@ -66,8 +73,8 @@ def generate_path(network, section1=None, section2=None, min_path_length=30000, 
     # The true data is intended for path visualization. There is only a one-to-one relationship between true data
     # points and offset data points if omit_factor == 1 (no data is omitted from offset data).
 
-    true_data = [util.Shapes.DataPoint(timestamp, network.node_speed_limit[v], network.node_locations[v][0],
-                                       network.node_locations[v][1], network.node_heading[v]) for v in vertex_path]
+    true_data = [DataPoint(timestamp, network.node_speed_limit[v], network.node_locations[v][0],
+                           network.node_locations[v][1], network.node_heading[v]) for v in vertex_path]
 
     # Keep only every xth point, where x = omit_factor, to simulate sparse data.
     try:
@@ -75,19 +82,18 @@ def generate_path(network, section1=None, section2=None, min_path_length=30000, 
         vertex_path = vertex_path[::omit_factor]
     except AssertionError:
         print("Omit factor too big.")
-        vertex_path = vertex_path[::omit_factor//2]
-
+        vertex_path = vertex_path[::omit_factor // 2]
 
     # Offset each vertex in the current path by a random amount within a given threshold.
-    offset_data = [util.Shapes.DataPoint(timestamp, network.node_speed_limit[v],
-                                         *util.Shapes.Point.as_tuple(
-                                             util.utils.offset_point(util.Shapes.Point(network.node_locations[v][0],
-                                                                                       network.node_locations[v][1],
-                                                                                       network.node_heading[v]),
-                                                                     random.randrange(max_offset_distance),
-                                                                     random.randrange(359)))[:2],
-                                         (network.node_heading[v] + random.randrange(-max_offset_heading,
-                                                                                     max_offset_heading)) % 360)
+    offset_data = [DataPoint(timestamp, network.node_speed_limit[v],
+                             *Point.as_tuple(
+                                 offset_point(Point(network.node_locations[v][0],
+                                                    network.node_locations[v][1],
+                                                    network.node_heading[v]),
+                                              (randrange(max_offset_distance) if random_distance
+                                               else max_offset_distance), randrange(359)))[:2],
+                             (network.node_heading[v] + offset_heading + randrange(-heading_noise,
+                                                                                   heading_noise) % 360))
                    for v in vertex_path]
     return offset_data, true_data
 
@@ -100,7 +106,7 @@ def export_artificial_data(offset_data, true_data, trip):
     :param trip: an integer representing the trip number
     :return: (list of header, list of dictionaries for artificial data, list of dictionaries for true data)
     """
-    data_header = ['probe_id', 'sample_date', 'lat', 'lon', 'heading', 'speed', 'probe_data_provider', 'trip_id']
+    data_header = ['PROBE_ID', 'SAMPLE_DATE', 'LAT', 'LON', 'HEADING', 'SPEED', 'PROBE_DATA_PROVIDER', 'TRIP_ID']
     artificial_data = [{'PROBE_ID': 0,
                         'SAMPLE_DATE': str(d.timestamp + datetime.timedelta(seconds=i)),
                         'LAT': d.lat,
@@ -123,7 +129,8 @@ def export_artificial_data(offset_data, true_data, trip):
 
 
 def multiple_artificial_paths(network, num_trips, section1=None, section2=None, min_path_length=30000,
-                              max_path_length=50000, max_offset_distance=200, max_offset_heading=25, omit_factor=1):
+                              max_path_length=50000, max_offset_distance=200, random_distance=True,
+                              offset_heading=10, heading_noise=5, omit_factor=1):
     """
     Generate multiple artificial paths at once, ready to exported to a .csv file. Each path will have a unique
     trip_id to distinguish it.
@@ -137,7 +144,7 @@ def multiple_artificial_paths(network, num_trips, section1=None, section2=None, 
 
     for i in range(num_trips):
         offset, true = generate_path(network, section1, section2, min_path_length, max_path_length, max_offset_distance,
-                                     max_offset_heading, omit_factor)
+                                     random_distance, offset_heading, heading_noise, omit_factor)
         header, fake_trip, real_trip = export_artificial_data(offset, true, i)
         fake_data.extend(fake_trip)
         real_data.extend(real_trip)
