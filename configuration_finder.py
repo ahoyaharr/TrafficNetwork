0e1a8481aa8c1e53265b8fbd500fd03418bfddb5
ahoyaharr <alexander.harrison@berkeley.edu>
2018-08-19 15:45:56 -0700

    docstrings added

--- configuration_finder.py
+++ configuration_finder.py
@@ -6,7 +6,17 @@ from functools import reduce
 
 
 class ConfigurationFinder:
+    """
+    The ConfigurationFinder class can construct randomly generated score functions and
+    test them against synthetic paths. A genetic algorithm can be used to find children
+    of the randomly generated score functions until a suitably fit child is found.
+    """
     def __init__(self, configuration_template, mm):
+        """
+        :param configuration_template: A tuple specifying each function type as a string.
+                                       example: ('manipulation', 'manipulation', 'value', 'combiner')
+        :param mm: A map matching object.
+        """
         self.function_factory = namedtuple('function_factory', ['outer', 'inner'])
         self.component_fn = namedtuple('component_fn', ['fn', 'name'])
         self.configuration = namedtuple('configuration', ['fn', 'as_text', 'age'])
@@ -60,6 +70,10 @@ class ConfigurationFinder:
                                  'reduction': self.function_factory(None, self.combiner_options)}
 
     def score_path(self, candidate, key):
+        """
+        Given a candidate path and it's solution, give the candidate path a score.
+        If d := the length of the longer of the two paths, the score is given by (d - edit_distance) / d.
+        """
         def levenshtein_distance(s, t):
             """
             Computes the Levenshtein distance between two paths.
@@ -98,7 +112,15 @@ class ConfigurationFinder:
 
         return (maximum_path_distance - edit_distance) / maximum_path_distance
 
-    def fn_builder(self, configuration, complexity, r, previous_function=None):
+    def new_fn_builder(self, configuration, complexity, r, previous_function=None):
+        """
+        Constructs a random new function.
+        :param configuration: The type of function to be constructed.
+        :param complexity: The number of levels of nesting in the function.
+        :param r: A collection of numbers. Arguments are selected from this value.
+        :param previous_function: Used for adding complexity, do not change unless you know what you are doing.
+        :return:
+        """
         if not self.factory_mappings[configuration].outer:  # Function does not chain, so select base fn and return.
             factory_option = random.choice(self.factory_mappings[configuration].inner)
             number_option = random.choice(r)
@@ -117,27 +139,46 @@ class ConfigurationFinder:
             new_fn = self.configuration(
                 factory_option.fn(previous_function.fn, number_option),
                 '{0}({1},{2})'.format(factory_option.name, previous_function.as_text, number_option), 0)
-        return self.fn_builder(configuration, complexity - 1, r, new_fn)
+        return self.new_fn_builder(configuration, complexity - 1, r, new_fn)
+
+    def child_fn_builder(self, existing_fn, k = 3, include_parent = True):
+        """
+        Given an existing function, return k children of that function.
+        :param existing_fn: A function wrapped in a configuration named tuple.
+        :param k: The number of offspring.
+        :param include_parent: Whether or not the parent value should be preserved.
+        :return:
+        """
+        pass
 
     def build_configuration(self, complexity=3, r=[i * .1 for i in range(-100, 100)]):
-        return (self.fn_builder(configuration, complexity, r) for configuration in self.configuration_template)
+        """
+        Construct a new random function for each function requested in the configuration template.
+        :param complexity:
+        :param r:
+        :return:
+        """
+        return (self.new_fn_builder(configuration, complexity, r) for configuration in self.configuration_template)
 
 
-value_config = ['value']
-manip_config = ['manipulation']
-combiner_config = ['reduction']
 
 
-def fakemm():
-    return
+def test():
+    value_config = ['value']
+    manip_config = ['manipulation']
+    combiner_config = ['reduction']
 
+    def fakemm():
+        return
 
-fakemm.network = []
+    fakemm.network = []
 
-v = ConfigurationFinder(value_config, fakemm)
-m = ConfigurationFinder(manip_config, fakemm)
-c = ConfigurationFinder(combiner_config, fakemm)
+    test.v = ConfigurationFinder(value_config, fakemm)
+    test.m = ConfigurationFinder(manip_config, fakemm)
+    test.c = ConfigurationFinder(combiner_config, fakemm)
 
-v_result = list(v.build_configuration(complexity=5))
-m_result = list(m.build_configuration(complexity=5))
-c_result = list(c.build_configuration(complexity=5))
+    test.v_result = list(v.build_configuration(complexity=5))
+    test.m_result = list(m.build_configuration(complexity=5))
+    test.c_result = list(c.build_configuration(complexity=5))
+
+test()
