fb06264da6a7fb3d856604d82172abd1aa1a53f2
Serena <sjhughes15@gmail.com>
2019-01-14 11:47:38 -0800

    added logic to ensure trip is not condensed to one point, code cleanup

--- util/clustering.py
+++ util/clustering.py
@@ -1,35 +1,12 @@
 import math
 import os, sys
 import datetime
+from util.utils import real_distance
 
 WINDOWS_ENCODING = '\\'
 UNIX_ENCODING = '/'
 
-SYSTEM_TYPE = 'windows'
-
-def real_distance(cp1, cp2):
-    """
-    >>> 995.0 <= real_distance([-118.121438, 34.179766], [-118.118132, 34.179786]) <= 1000.0
-    True
-
-    Computes the distance in feet between two points using the Haversine Formula.
-    :param cp1: A list in the form [lon1, lat1].
-    :param cp2: A list in the form [lon2, lat2].
-    :return: The distance in feet between two coordinates.
-    """
-    earth_radius = 6378.1
-    KM_TO_FEET_CONST = 3280.84
-
-    cp1 = list(map(math.radians, cp1))
-    cp2 = list(map(math.radians, cp2))
-
-    delta_lon = cp2[0] - cp1[0]
-    delta_lat = cp2[1] - cp1[1]
-
-    a = math.sin(delta_lat / 2) ** 2 + math.cos(cp1[1]) * math.cos(cp2[1]) * math.sin(delta_lon / 2) ** 2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    return earth_radius * c * KM_TO_FEET_CONST
+SYSTEM_TYPE = 'linux'
 
 
 def cluster_compare(prev, cur, dist_thres=200, time_thres=45):
@@ -38,8 +15,8 @@ def cluster_compare(prev, cur, dist_thres=200, time_thres=45):
     A helper function to determine whether or not the current point can be clustered, given the previous.
     Returns True if we should keep both points, otherwise False.
     """
-    cur_time = datetime.datetime.strptime(cur[1],  "%Y-%m-%d %H:%M:%S")
-    prev_time = datetime.datetime.strptime(prev[1],  "%Y-%m-%d %H:%M:%S")
+    cur_time = datetime.datetime.strptime(cur[1], "%Y-%m-%d %H:%M:%S")
+    prev_time = datetime.datetime.strptime(prev[1], "%Y-%m-%d %H:%M:%S")
     time_delta = (cur_time - prev_time).seconds
     dist_delta = real_distance((float(prev[3]), float(prev[2])), (float(cur[3]), float(cur[2])))
     if (time_delta >= time_thres) or (dist_delta >= dist_thres):
@@ -59,7 +36,7 @@ def cluster_file(subdir, file):
     f = open(os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + subdir + separator() + file)
     contents = f.read().splitlines()
     header = contents[0]
-    clustered_contents = []
+    clustered_contents = list()
     clustered_contents.append(header + '\n')
     
     prev_line = contents[1]
@@ -67,19 +44,22 @@ def cluster_file(subdir, file):
     for line in contents[2:]:
         cur = line.split(',')
         if prev[-1] == cur[-1]:
-            print("OLD TRIP Prev:", prev[-1], "OLD TRIP Cur:", cur[-1])
+            # print("OLD TRIP Prev:", prev[-1], "OLD TRIP Cur:", cur[-1])
             if cluster_compare(prev, cur):
                 clustered_contents.append(line + '\n')
                 prev = cur
         else:
             # new trip
-            print("NEW TRIP Prev:", prev[-1], "NEW TRIP Cur:", cur[-1])
+            # print("NEW TRIP Prev:", prev[-1], "NEW TRIP Cur:", cur[-1])
             clustered_contents.append(line + '\n')
             prev = cur
-    clustered_file = os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + subdir + separator() + "clustered_" + file
-    with open (clustered_file, 'w') as text_file:
-        text_file.write("".join(clustered_contents))
-    return clustered_file
+
+    if len(clustered_contents) > 3:
+        clustered_file = os.path.dirname(os.path.realpath(sys.argv[0])) + separator() + subdir + separator() + "clustered_" + file
+        with open (clustered_file, 'w') as text_file:
+            text_file.write("".join(clustered_contents))
+        return clustered_file
+    return None
 
 
 def get_files(path, absolute=False):
@@ -106,6 +86,9 @@ def read_all(subdirectory):
     for file in get_files(subdirectory):
         print('\tClustering ' + file)
         clustered_file = cluster_file(subdirectory, file)
-        print('\tWrote corrected dataset to ' + clustered_file)
+        if clustered_file:
+            print('\tWrote corrected dataset to ' + clustered_file)
+        else:
+            print('\tTrip too short. Not written.')
 
-read_all('data')
+read_all('to_cluster')
